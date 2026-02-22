bf92c466c593a207475c551d220eaf308f4edd13
Olivier Proulx <oproulx8896@gmail.com>
2026-02-21 20:09:20 -0600

    spline like a water slide

--- Assets/FadeInImageOnStart.cs
+++ Assets/FadeInImageOnStart.cs
@@ -1,8 +1,11 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FadeInImageOnStart : MonoBehaviour
 {
+    public CinemachineSplineDolly dolly;
+    public AnimationCurve animationCurve = AnimationCurve.EaseInOut(0,0,1,1);
     public Image image;
     public float speed = 3;
     public float lerpTime = 0;
@@ -16,9 +19,10 @@ public class FadeInImageOnStart : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        lerpTime += Mathf.Min(1, lerpTime + Time.deltaTime/speed);
+        lerpTime = Mathf.Min(2, lerpTime + Time.deltaTime/speed);
 
-        image.color = new Color( image.color.r, image.color.g, image.color.b, 1-lerpTime);
+        image.color = new Color( image.color.r, image.color.g, image.color.b, 1-animationCurve.Evaluate(lerpTime));
+        dolly.CameraPosition = animationCurve.Evaluate(lerpTime/2);
 
     }
 }
