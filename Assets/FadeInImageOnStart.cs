fa1e5e19762428c4f3e25dae40437b2f6eeddd5b
Olivier Proulx <oproulx8896@gmail.com>
2026-02-22 13:49:21 -0600

    its like giving birth

--- Assets/FadeInImageOnStart.cs
+++ Assets/FadeInImageOnStart.cs
@@ -4,11 +4,15 @@ using UnityEngine.UI;
 
 public class FadeInImageOnStart : MonoBehaviour
 {
+    public CinemachineCamera startCam;
+    public CinemachineCamera[] cameras;
+    private CinemachineCamera usingCamera;
     public CinemachineSplineDolly dolly;
     public AnimationCurve animationCurve = AnimationCurve.EaseInOut(0,0,1,1);
     public Image image;
     public float speed = 3;
     public float lerpTime = 0;
+    private bool startingState = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,8 +26,21 @@ public class FadeInImageOnStart : MonoBehaviour
     {
         lerpTime = Mathf.Min(2, lerpTime + Time.deltaTime/speed);
 
-        image.color = new Color( image.color.r, image.color.g, image.color.b, 1-animationCurve.Evaluate(lerpTime));
-        dolly.CameraPosition = animationCurve.Evaluate(lerpTime/2);
+        if (startingState)
+        {
+            image.color = new Color( image.color.r, image.color.g, image.color.b, 1-animationCurve.Evaluate(lerpTime));
+            dolly.CameraPosition = animationCurve.Evaluate(lerpTime/2);
+
+            if(lerpTime >= 2) startingState = false;
+
+            return;
+        }
+
+        if(lerpTime >= 2)
+        {
+            
+        }
+
 
     }
 }
