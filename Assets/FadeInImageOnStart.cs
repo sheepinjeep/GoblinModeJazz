fa1a7d73c51b237abd1d08a46f264d7acc95b5da
Olivier Proulx <oproulx8896@gmail.com>
2026-02-22 14:09:27 -0600

    Camera Moves

--- Assets/FadeInImageOnStart.cs
+++ Assets/FadeInImageOnStart.cs
@@ -13,34 +13,41 @@ public class FadeInImageOnStart : MonoBehaviour
     public float speed = 3;
     public float lerpTime = 0;
     private bool startingState = true;
+    private float funnyLerpFactor = 2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         image = GetComponent<Image>();
         image.enabled = true;
+        startCam.Prioritize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        lerpTime = Mathf.Min(2, lerpTime + Time.deltaTime/speed);
+        lerpTime = Mathf.Min(funnyLerpFactor, lerpTime + Time.deltaTime/speed);
 
         if (startingState)
         {
             image.color = new Color( image.color.r, image.color.g, image.color.b, 1-animationCurve.Evaluate(lerpTime));
-            dolly.CameraPosition = animationCurve.Evaluate(lerpTime/2);
+            dolly.CameraPosition = animationCurve.Evaluate(lerpTime/funnyLerpFactor);
 
-            if(lerpTime >= 2) startingState = false;
+            if(lerpTime >= funnyLerpFactor) startingState = false;
 
             return;
         }
 
         if(lerpTime >= 2)
         {
-            
+            lerpTime = 0;
+            usingCamera = cameras[Random.Range(0,cameras.Length)];
+            dolly = usingCamera.GetComponent<CinemachineSplineDolly>();
+            usingCamera.Prioritize();
+            speed = Random.Range(4f,8f);
         }
-
+        if(usingCamera != startCam)
+            dolly.CameraPosition = animationCurve.Evaluate(lerpTime/funnyLerpFactor);
 
     }
 }
